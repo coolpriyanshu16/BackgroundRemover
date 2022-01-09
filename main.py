a6b8c8d8e25cbf834718b46cac92d1dff3cb17f7
Priyanshu Raj <coolpriyanshu16@gmail.com>
2022-01-09 20:49:58 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+import cv2
+import cvzone
+
+from cvzone.SelfiSegmentationModule import SelfiSegmentation
+import os
+
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)  # width
+cap.set(4, 480)  # Height
+cap.set(cv2.CAP_PROP_FPS, 60)
+#create selfie segmentation object
+segmentor = SelfiSegmentation()
+fpsReader = cvzone.FPS()
+
+
+
+listImg = os.listdir("Images")
+print(listImg)
+imgList = []
+
+for imgPath in listImg:
+    img = cv2.imread(f'Images/{imgPath}')
+    imgList.append(img)
+print(len(imgList))
+
+
+indexImg = 0
+
+
+while True:
+    success, img = cap.read()
+    imgOut = segmentor.removeBG(img,imgList[indexImg],threshold=0.8)
+
+    imgStacked = cvzone.stackImages([img,imgOut],2,1)
+    _, imgStacked = fpsReader.update(imgStacked,color=(0,0,255))
+    cv2.imshow("Image",imgStacked)
+    # cv2.imshow("Image",img)
+    # cv2.imshow("Image Out",imgOut)
+    key = cv2.waitKey(1)
+    if key ==ord('a'):
+        if indexImg > 0:
+            indexImg -= 1
+
+    elif key == ord('d'):
+        if indexImg < len(imgList) -1 :
+            indexImg += 1
+    elif key == ord('q'):
+        break
+
+    #cv2.waitKey(1)
